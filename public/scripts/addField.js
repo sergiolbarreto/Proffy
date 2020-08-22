5f23b6792cd75464acc5c884e6158c154d1a7289
Sérgio Barreto <70080558+sergiolbarreto@users.noreply.github.com>
2020-08-22 10:58:47 -0300

    files

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -1,11 +1,7 @@
 // Procurar o botão
 document.querySelector("#add-time")
 //Quando clicar no botão
-.addEventListener("click", cloneField) //acessar o botão "novo horário", usou o add-time porque é a id do botão
- //essa função que na tradução literal é o escutador de eventos, recebe dois argumentos,
-//o primeiro, que é o que ela vai receber, que no caso é quando o usuário clicar e o que ela vai fazer que é ativar o cloneField
-//porém, não existe essa função nesse documento, então temos que criá-la abaixo.
-
+.addEventListener("click", cloneField) 
 //Executar uma ação
 function cloneField() { // criar uma função
     //Duplicar os campos. Que campos?
